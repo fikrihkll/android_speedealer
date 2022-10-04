5e5f0a60eca692f98c0826d0bc105b8f92f949cb
zackmolt <fkrihkl@gmail.com>
2022-10-05 00:27:28 +0700

    setup header home

---
+++ app/src/main/java/com/teamdagger/speedealer/home/HomeActivity.kt
@@ -0,0 +1,76 @@
+package com.teamdagger.cardealer.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamdagger.speedealer.ui.theme.SpeeDealerTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SpeeDealerTheme {
+                Screen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Screen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background,
+    ) {
+        Header()
+    }
+}
+
+@Composable
+fun Header() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 32.dp),
+        color = MaterialTheme.colors.background,
+    ) {
+        SearchTitle()
+    }
+}
+
+@Composable
+fun SearchTitle() {
+    Row {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = "Explore"
+        )
+        Icon(
+            Icons.Default.Search,
+            contentDescription = null // decorative element
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    SpeeDealerTheme {
+        Screen()
+    }
+}
